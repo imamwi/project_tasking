2672e98604dbf27a3c731b73fb11fdec14a4e244
imamwi <imamwynt@gmail.com>
2022-08-11 16:42:09 +0700

    tambah views base.html dan index,html

--- controllers/NoteControllers.go
+++ controllers/NoteControllers.go
@@ -1,14 +1,42 @@
 package controllers
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type NoteControllers struct{}
 
 func (controller *NoteControllers) Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "welcome mam")
+	_, err := gorm.Open(sqlite.Open("data.db", &gorm.Config{}))
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	files := []string{
+		".views/base.html",
+		"./views/index.html",
+	}
+
+	htmlTemplate, err := template.ParseFiles(files...)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	datas := map[string]interface{}{}
+
+	err = htmlTemplate.ExecuteTemplate(w, "base", datas)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err.Error())
+	}
 }
